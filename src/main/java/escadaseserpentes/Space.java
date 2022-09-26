6d45a06cd2e426476d817aa9e15fbb9763f2bc6d
Mateus Dantas <mateusdantas.dev@gmail.com>
2022-09-26 11:02:35 -0300

    Criação da classe Space.

--- src/main/java/escadaseserpentes/Space.java
+++ src/main/java/escadaseserpentes/Space.java
@@ -1,4 +1,29 @@
 package escadaseserpentes;
 
 public class Space {
+	
+	public enum Type {
+		START_HERE, HOME, REGULAR;
+	}
+	
+	private Type type;
+	private int number;
+	
+	public Space(Type type, int number) {
+		this.type = type;
+		this.number = number;
+	}
+	
+	public Type getType() {
+		return type;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public String toString() {
+		return "Space [type=" + type + ", number=" + number + "]";
+	}
 }
