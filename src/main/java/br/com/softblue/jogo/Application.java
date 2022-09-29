337d400f6fc1d859aa6507e6b82027ecd95905f6
Mateus Dantas <mateusdantas.dev@gmail.com>
2022-09-29 19:15:00 -0300

    Corrige Readme

--- src/main/java/br/com/softblue/jogo/Application.java
+++ src/main/java/br/com/softblue/jogo/Application.java
@@ -2,17 +2,11 @@ package br.com.softblue.jogo;
 
 import br.com.softblue.jogo.core.Game;
 
-import javax.swing.*;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         Game game = new Game();
-
         game.play();
 
     }
